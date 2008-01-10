0bd8e0a74cd928884ee22e3fcd4c5136cfb51bfd
piotr.roszatycki <piotr.roszatycki>
2008-01-10 13:55:29 +0000

    * Add SetUpTearDownTest

---
+++ t/tlib/SetUpTearDownTest.pm
@@ -0,0 +1,26 @@
+package SetUpTearDownTest;
+
+use strict;
+use warnings;
+
+use base 'Test::Unit::TestCase';
+
+my $a;
+
+sub set_up {
+  $a = 1;
+}
+
+sub test_inc {
+  my $self = shift;
+  $a++;
+  $self->assert_equals(2, $a);
+}
+
+sub test_dec {
+  my $self = shift;
+  $a--;
+  $self->assert_equals(0, $a);
+}
+
+1;
